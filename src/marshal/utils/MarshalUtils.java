7be75a1a1e367fa441ba4f068e105a4e9f2a55d4
A <>
2021-05-07 11:24:33 +0200

    Update utils

--- src/marshal/utils/MarshalUtils.java
+++ src/marshal/utils/MarshalUtils.java
@@ -1,5 +1,9 @@
 package marshal.utils;
 
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.List;
+
 public class MarshalUtils {
 
 	public static void printBytes(byte[] bytes) {
@@ -8,6 +12,15 @@ public class MarshalUtils {
 			System.out.print(st + " ");
 		}
 		System.out.println("");
+	}
+	
+	public static void writeCSV(List<String> data, String filename) throws IOException {
+		FileWriter csvWriter = new FileWriter(filename);
+		csvWriter.append("Nb_messages,Execution_time\n");
+		for(String line : data)
+			csvWriter.append(line);
+		csvWriter.flush();
+		csvWriter.close();
 	}
 	
 }
